7e0feaa1864f56af7f9eedcb07a6b78ecb616c35
Hemadri <108988522+chemadri@users.noreply.github.com>
2023-03-08 23:32:50 +0530

    react progress value updated.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navbarOffsetTop = navbar.offsetTop;
 const sections = document.querySelectorAll("section");
 const navbarLinks = document.querySelectorAll(".navbar-link");
 const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [90, 75, 70, 70, 50, 50, 50];
+const progressBarPercents = [90, 75, 70, 70, 50, 70, 50];
 
 window.addEventListener("scroll", () => {
   mainFn();
